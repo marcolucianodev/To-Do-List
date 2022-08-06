aeefdc4dc8013232632e25ee38505e7a531b057a
marcolucianodev <maurelio.sluciano@gmail.com>
2022-08-06 11:27:20 -0300

    create Home page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-import React, { useState } from 'react';
 import './App.css';
-import Form from './components/Form';
-import ListItem from './components/ListItem';
+import Home from './pages/Home';
 
 
 const App = () => {
-  
-  const [lists, setLists] = useState([]);
-
-  const handleTaskAdd = (list) => {
-    setLists([...lists, list])
-    console.log(list)
-  }
-
-  const deleteTask = (id) => {
-    console.log(id)
-    let filteredId = lists.filter((list) => list.id !== id)
-    setLists(filteredId)
-  }
-  
+    
   return (
     <>
-      <Form handleTaskAdd={handleTaskAdd} />
-        <div style={{backgroundColor: "red"}}>
-          {lists.map((list) => (
-            <ListItem list={list} key={list.id} deleteTask={deleteTask}/>
-          ))}
-        </div>    
+      <Home />
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Form from '../components/Form';
+import ListItem from '../components/ListItem';
+
+const Home = () => {
+
+  const [lists, setLists] = useState([]);
+
+  const handleTaskAdd = (list) => {
+    setLists([...lists, list])
+    console.log(list)
+  }
+
+  const deleteTask = (id) => {
+    console.log(id)
+    let filteredId = lists.filter((list) => list.id !== id)
+    setLists(filteredId)
+  }
+
+  return (
+    <>
+      <Form handleTaskAdd={handleTaskAdd} />
+        <div style={{backgroundColor: "red"}}>
+          {lists.map((list) => (
+            <ListItem list={list} key={list.id} deleteTask={deleteTask}/>
+          ))}
+        </div>    
+    </>
+  );
+}
+
+export default Home;
