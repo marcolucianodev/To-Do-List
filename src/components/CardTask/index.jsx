c466f64f076c11aba2e2fc0b99216ee0bab97161
marcolucianodev <maurelio.sluciano@gmail.com>
2022-08-09 19:12:38 -0300

    update styles and clear code

--- src/components/CardTask/index.jsx
+++ src/components/CardTask/index.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
-import { Button, FormContainer, InputText } from "../../pages/styles";
+import { 
+  Button, 
+  FormContainer, 
+  InputText 
+} from "../../pages/styles";
 import AddIcon from '@mui/icons-material/Add';
-import DeleteIcon from '@mui/icons-material/Delete';
-import { ListContent, ListItems } from "./styles";
+import { 
+  ListContent, 
+  ListItems 
+} from "./styles";
 import deleteIcon from "../../assets/images/trash-icon.png"
 
 
-const CardTask = ({deletecard}) => {
+const CardTask = () => {
 
   const [list, setList] = useState([]);
   const [newItem, setNewItem] = useState("");
@@ -40,26 +46,15 @@ const CardTask = ({deletecard}) => {
         <AddIcon />
       </Button>
     </FormContainer>
-      {/* <input type="text" value={newItem} onChange={(e) => setNewItem(e.target.value)} />
-      <button  onClick={() => addNewItem()}>+ Add</button> */}
-      
-      {/* trabalando botao DELETAR CARD */}
-
-      {/* <button onClick={deletecard}>Botao deletar</button> */}
-      
-      {/* FIM */}
-
 
       <ListContent>
         {list.map((item, index) => (
           <ListItems key={index}>
             {item}
-            {/* <button onClick={() => deleteItem(index)}>X</button> */}
             <Button 
               onClick={() => deleteItem(index)}
               bgButton="transparent"
             >
-              {/* <DeleteIcon className="delete-icon"/> */}
               <img src={deleteIcon} alt="Delete" />
             </Button>
           </ListItems>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
 import { useState } from "react";
 import CardTask from "../components/CardTask";
-import { Button, CardContent, CardHead, CardItems, MainContent, FormContainer, InputText, Container } from "./styles";
+import { 
+  Button,
+  CardContent,
+  CardHead, 
+  CardItems, 
+  MainContent, 
+  FormContainer, 
+  InputText, 
+  Container 
+} from "./styles";
 import AddIcon from '@mui/icons-material/Add';
-// import DeleteIcon from '@mui/icons-material/Delete';
 import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
 import Header from "../components/Header";
 import Footer from "../components/Footer";
@@ -10,37 +18,19 @@ import deleteIcon from "../assets/images/trash-icon.png"
 
 const Home = () => {
 
-  // const [list, setList] = useState([]);
-  // const [newItem, setNewItem] = useState("");
-
   const [card, setCard] = useState([]);
   const [newCard, setNewCard] = useState("");
-
-  // const addNewItem = () => {
-  //   setList([...list, newItem]);
-  //   setNewItem("");
-  // }
 
   const addNewCard = () => {
     setCard([...card, newCard]);
     setNewCard("");
   }
 
-  // const deleteItem = (index) => {
-  //   const tempArr = [...list];
-  //   tempArr.splice(index, 1)
-
-  //   setList(tempArr);
-  // }
-
   const deleteCard = (index) => {
     const tempCard = [...card]
     tempCard.splice(index, 1)
 
     setCard(tempCard)
-    console.log('DELETE CARD')
-    console.log(index)
-
   }
 
   return (
@@ -64,8 +54,6 @@ const Home = () => {
           </Button>
         </FormContainer>
         
-      {/* <input type="text" value={newCard} onChange={(e) => setNewCard(e.target.value)} /> */}
-      {/* <button  onClick={() => addNewCard()}>Add</button> */}
         <CardContent>
           {card.map((card, index) => (
             <CardItems key={index}>
@@ -78,7 +66,6 @@ const Home = () => {
                   onClick={() => deleteCard(index)}
                   bgButton="transparent"
                 >
-                  {/* <DeleteIcon className="delete-icon"/> */}
                   <img src={deleteIcon} alt="Delete" />
                 </Button>
               </CardHead>
